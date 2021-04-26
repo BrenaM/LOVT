2bba8d59849fbddf1ff5d213a5a4b7c23be0fea3
BrenaM <brenamonteiro_@hotmail.com>
2021-04-26 13:13:28 -0300

    refatorando usando o DOM

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//console.log('JS externo funcionol!');
+// console.log(document);
+//console.log(window);
+
+//alert('Seja bem vindo!') //msg para usuário
+
+//let adult = confirm('Você tem mais de 18 anos?');  //retorna booleano
+
+//let nomeUsuario = prompt('Qual o seu nome?'); //retorna texto inserio pelo user
+
+// console.log(nomeUsuario);
+
+// let elementHeader = document.querySelector('.olaUsuario'); //seleciona o elemneto html
+
+// console.log(elementHeader);
+// //elementHeader.innerText = `Olá, ${nomeUsuario}`; //atribui conteudo ao elemento selecionado
+
+// // elementHeader.innerHTML += `<strong> Olá, ${nomeUsuario}</strong>`;
+
+// let logo = document.getElementById('logo');
+// console.log(logo);
+
+//logo.style.color = "orange";
+
+const toggleMode = () => {
+    document.body.classList.toggle("dark-mode");
+}
+
