e9fda05ed149062ed8e1e25a49be94ddb7840449
BrenaM <brenamonteiro_@hotmail.com>
2021-04-27 14:17:23 -0300

    formulario com JS

--- js/main.js
+++ js/main.js
@@ -32,3 +32,53 @@ setTimeout( () => {
 const btnClose = () => {
     document.querySelector('.modal').style.display = "none";
 }
+
+
+// regras para o form
+// document.querySelector("#btn-submit-contact").addEventListener("click", (evento) => {
+//     evento.preventDefault(); //cancelando o envio temporariamente
+
+//     let nomeValue = document.querySelector("#input_nome").value;
+//     let emailValue = document.querySelector("#input_email").value;
+
+//     console.log(nomeValue);
+//     console.log(emailValue);
+//     //validação dos campos
+//     //exibição de msg de erro
+//     //envio do form
+
+// });
+
+let formContact = document.querySelector("#form_contact");
+formContact.addEventListener("submit", (evento) => {
+    //interrrompo o envio do formulario
+    evento.preventDefault();
+
+        let nomeValue = document.querySelector("#input_nome").value;
+        let emailValue = document.querySelector("#input_email").value;
+        let telefoneValue = document.querySelector("#input_telefone").value;
+        let mensagemValue = document.querySelector("#input_mensagem").value;
+        let divMensagemErro = document.querySelector(".mensagem-erro");
+        
+        // validar todos os campos estão preenchido
+        //validar se nome tem 2 ou mais caracteres
+        //validar se telegone tem no minimo 8 caracteres
+        //verificar se o campo email tem @ (google - validação c/ regex)
+        if (nomeValue.length < 2 || emailValue.length < 1 || telefoneValue.length < 1 || mensagemValue.length < 1) {
+            divMensagemErro.innerHTML += "Preencha todos os campos. <br>";
+            // alert("Preencha todos os campos")
+            return;
+        }else if (telefoneValue.length < 8) {
+            divMensagemErro.innerHTML += "Telefone deve conter pelo menos 8 digitos. <br>";
+        }else if (emailValue.indexOf("@") < 0) {
+            divMensagemErro.innerHTML = "Insira um email válida. <br>";
+        } else {
+            formContact.submit();
+        }
+         
+        // const validateEmail = (email) => {
+        //     const re = /\S+@\S+\.\S+/;
+        //     return re.test(email);
+        //   };
+    
+});
